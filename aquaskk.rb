75e333b514cb5188ae23889d9e5c507f06a2499d
papaeye <papaeye@gmail.com>
2015-01-15 22:16:10 +0900

    aquaskk: Fix sticky-key to sticky-shift

--- aquaskk.rb
+++ aquaskk.rb
@@ -4,9 +4,9 @@ class Aquaskk < Formula
   homepage 'http://aquaskk.sourceforge.jp/'
   head 'https://github.com/codefirst/aquaskk.git'
 
-  option 'with-sticky-key', 'Build with sticky-key support'
+  option 'with-sticky-shift', 'Build with sticky-shift support'
 
-  if build.with? 'sticky-key'
+  if build.with? 'sticky-shift'
     patch do
       url 'https://gist.github.com/papaeye/8368908/raw/6053615ae23736baaf24a566d837d9cf5b7bdd79/aquaskk-sticky-key.patch'
       sha1 '5dfa557527d650ff8ac1aa99668ae0cc69e07eec'
