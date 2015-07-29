8860a25992eac86901d3d97fd5be502aed6b741f
papaeye <papaeye@gmail.com>
2015-07-30 02:48:28 +0900

    Emacs 24.5 Mac port 5.9

--- emacs-mac-port.rb
+++ emacs-mac-port.rb
@@ -5,11 +5,11 @@ class EmacsMacPort < Formula
     url "http://ftpmirror.gnu.org/emacs/emacs-24.5.tar.xz"
     mirror "https://ftp.gnu.org/pub/gnu/emacs/emacs-24.5.tar.xz"
     sha256 "dd47d71dd2a526cf6b47cb49af793ec2e26af69a0951cc40e43ae290eacfc34e"
-    version "24.5-mac-5.8"
+    version "24.5-mac-5.9"
 
     resource "mac-port" do
-      url "ftp://ftp.math.s.chiba-u.ac.jp/emacs/emacs-24.5-mac-5.8.tar.gz"
-      sha256 "3942a62479541ab93522cd7ba8f45860e366699aaf6ad7c589e8295a9eca5052"
+      url "ftp://ftp.math.s.chiba-u.ac.jp/emacs/emacs-24.5-mac-5.9.tar.gz"
+      sha256 "19f3a9d223564667d62e82263075c21c6e04d1dceb6ba8b10dd15a2a9f9a5af6"
     end
   end
 
